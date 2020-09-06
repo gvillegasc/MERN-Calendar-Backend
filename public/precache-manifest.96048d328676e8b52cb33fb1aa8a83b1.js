7c711dd5f1e9331db6158fe277e49250b2f55b82
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-06 16:48:32 -0500

    v1 Calendar A´´

---
+++ public/precache-manifest.96048d328676e8b52cb33fb1aa8a83b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b08177abb901ed67ce2ccbe3952086c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b618b0c927ab4f0937",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "be2800b4b9be4133b615",
+    "url": "/static/css/main.f65ddf3e.chunk.css"
+  },
+  {
+    "revision": "84b618b0c927ab4f0937",
+    "url": "/static/js/2.c5e730fb.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.c5e730fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2800b4b9be4133b615",
+    "url": "/static/js/main.ab4fc021.chunk.js"
+  },
+  {
+    "revision": "3d99dbe3c7391d5aaa26",
+    "url": "/static/js/runtime-main.13f20f94.js"
+  }
+]);
